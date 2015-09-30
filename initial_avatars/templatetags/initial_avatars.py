7c839b3210b69310715bda9d94c401500866ed18
Mathieu RÉQUILLART <mrequillart@axiome.io>
2015-10-01 01:12:52 +0200

    fix(templatetags): return null image tag with the good size if no user found

--- initial_avatars/templatetags/initial_avatars.py
+++ initial_avatars/templatetags/initial_avatars.py
@@ -12,5 +12,8 @@ def get_initial_avatar(user_or_email, size=GRAVATAR_DEFAULT_SIZE):
         email = user_or_email.email
     else:
         email = user_or_email
-    user = User.objects.get(email=email)
+    try:
+        user = User.objects.get(email=email)
+    except User.DoesNotExists:
+        return "<img>src="" width="{width}" height="{height}"/>".format(width=size, height=size)
     return AvatarGenerator(user, size=int(size)).get_avatar()
