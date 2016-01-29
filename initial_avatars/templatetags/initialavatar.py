33656b541c703e6c2c8ff06c4feb543a5c0c3aa1
Mathieu Réquillart <mrequillart@axiome.io>
2016-01-29 17:23:22 +0100

    fix: add # -*- coding: utf-8 -*- at beginning of files to prevent bug when username has accent

--- initial_avatars/templatetags/initialavatar.py
+++ initial_avatars/templatetags/initialavatar.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.contrib.auth.models import User
 from django import template
 from initial_avatars.generator import AvatarGenerator, GRAVATAR_DEFAULT_SIZE
--- initial_avatars/views.py
+++ initial_avatars/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
