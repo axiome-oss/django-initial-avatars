cd29e2c0f1a53b7f24599bcbd7750e9600b646e0
Mathieu Réquillart <mrequillart@axiome.io>
2016-02-08 18:24:08 +0100

    add shape selection to templatetag (default to square)

--- initial_avatars/templatetags/initialavatar.py
+++ initial_avatars/templatetags/initialavatar.py
@@ -2,13 +2,13 @@
 from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django import template
-from ..generator import AvatarGenerator, GRAVATAR_DEFAULT_SIZE
+from ..generator import AvatarGenerator, GRAVATAR_DEFAULT_SIZE, AVATAR_SHAPE
 
 register = template.Library()
 
 
 @register.simple_tag(name='get_initial_avatar')
-def get_initial_avatar(user_or_email, size=GRAVATAR_DEFAULT_SIZE):
+def get_initial_avatar(user_or_email, size=GRAVATAR_DEFAULT_SIZE, shape=AVATAR_SHAPE):
     """ Builds an avatar <img> tag from an user or email """
 
     if hasattr(user_or_email, 'email'):
@@ -19,4 +19,4 @@ def get_initial_avatar(user_or_email, size=GRAVATAR_DEFAULT_SIZE):
         user = User.objects.get(email=email)
     except User.DoesNotExist:
         return '<img src="" width="{width}" height="{height}"/>'.format(width=size, height=size)
-    return AvatarGenerator(user, size=int(size)).get_avatar()
+    return AvatarGenerator(user, size=int(size), shape=shape).get_avatar()
