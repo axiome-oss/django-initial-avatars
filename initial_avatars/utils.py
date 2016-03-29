e438d92cb1f19274dff6e652f9811858b2538b06
Mathieu Réquillart <mrequillart@gmail.com>
2016-03-29 23:42:45 +0200

    add a setting to use a custom avatar generator

--- initial_avatars/utils.py
+++ initial_avatars/utils.py
@@ -1,9 +1,21 @@
+from django.conf import settings
+from importlib import import_module
+
+
 class AvatarShapeException(BaseException):
     pass
+
 
 class AvatarForegroundColorException(BaseException):
     pass
 
+
+def get_avatar_backend():
+    backend_name = getattr(settings, 'AVATAR_GENERATOR_BACKEND', 'initial_avatars.generator.AvatarGenerator')
+    backend_module_name, backend_cls_name = backend_name.rsplit('.', 1)
+    backend_module = import_module(backend_module_name)
+    backend_class = getattr(backend_module, backend_cls_name)
+    return backend_class
 
 AVATAR_SHAPE_SETTINGS = {
     'circle': {
