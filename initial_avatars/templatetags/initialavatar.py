5d1cd634f1cc0b2b90aae22bbbf8d30b8354a24a
Mathieu Réquillart <mrequillart@gmail.com>
2016-03-29 23:43:26 +0200

    use the AVATAR_GENERATOR_BACKEND setting in templatetags and views

--- initial_avatars/templatetags/initialavatar.py
+++ initial_avatars/templatetags/initialavatar.py
@@ -3,7 +3,8 @@ from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django.utils.safestring import mark_safe
 from django import template
-from ..generator import AvatarGenerator, GRAVATAR_DEFAULT_SIZE, AVATAR_SHAPE
+from ..generator import GRAVATAR_DEFAULT_SIZE, AVATAR_SHAPE
+from ..utils import get_avatar_backend
 
 register = template.Library()
 
@@ -20,4 +21,5 @@ def get_initial_avatar(user_or_email, size=GRAVATAR_DEFAULT_SIZE, shape=AVATAR_S
         user = User.objects.get(email=email)
     except User.DoesNotExist:
         return mark_safe('<img src="" width="{width}" height="{height}"/>'.format(width=size, height=size))
-    return mark_safe(AvatarGenerator(user, size=int(size), shape=shape).get_avatar())
+    avatar_backend = get_avatar_backend()
+    return mark_safe(avatar_backend(user, size=int(size), shape=shape).get_avatar())
