dc7d0e18ba29700d7617c80ea3ad54b36802e91d
Mathieu RÉQUILLART <mrequillart@axiome.io>
2016-09-10 18:25:25 +0200

    feat: add support for django 1.10

--- initial_avatars/urls.py
+++ initial_avatars/urls.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.conf.urls import url
+from .views import avatar
 
 urlpatterns = [
-    url(r'^(?P<id>[0-9]+)/$', 'initial_avatars.views.avatar', name='avatar'),
-    url(r'^(?P<id>[0-9]+)/(?P<size>[0-9]{2,3})/$', 'initial_avatars.views.avatar', name='avatar'),
+    url(r'^(?P<id>[0-9]+)/$', avatar, name='avatar'),
+    url(r'^(?P<id>[0-9]+)/(?P<size>[0-9]{2,3})/$', avatar, name='avatar'),
 ]
