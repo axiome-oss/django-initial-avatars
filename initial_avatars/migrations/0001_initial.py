e29324352dc21856840a568b484ff7bbd704d28f
Mathieu RÉQUILLART <mrequillart@axiome.io>
2018-01-28 19:05:53 +0100

    fix: migrations for django 2.0

--- initial_avatars/migrations/0001_initial.py
+++ initial_avatars/migrations/0001_initial.py
@@ -5,25 +5,38 @@ from django.db import migrations, models
 from django.conf import settings
 import django
 
+
 class Migration(migrations.Migration):
 
     if django.VERSION < (1, 8, 0):
         dependencies = [
             ('auth', '__first__'),
-    ]
+        ]
     else:
         dependencies = [
             ('auth', '0006_require_contenttypes_0002'),
         ]
-
-    operations = [
-        migrations.CreateModel(
-            name='Background',
-            fields=[
-                ('user', models.OneToOneField(related_name='initial_avatars', primary_key=True, serialize=False, to=settings.AUTH_USER_MODEL)),
-                ('R', models.IntegerField()),
-                ('G', models.IntegerField()),
-                ('B', models.IntegerField()),
-            ],
-        ),
-    ]
+    if django.VERSION < (2, 0, 0):
+        operations = [
+            migrations.CreateModel(
+                name='Background',
+                fields=[
+                    ('user', models.OneToOneField(related_name='initial_avatars', primary_key=True, serialize=False, to=settings.AUTH_USER_MODEL)),
+                    ('R', models.IntegerField()),
+                    ('G', models.IntegerField()),
+                    ('B', models.IntegerField()),
+                ],
+            ),
+        ]
+    else:
+        operations = [
+            migrations.CreateModel(
+                name='Background',
+                fields=[
+                    ('user', models.OneToOneField(related_name='initial_avatars', primary_key=True, serialize=False, to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)),
+                    ('R', models.IntegerField()),
+                    ('G', models.IntegerField()),
+                    ('B', models.IntegerField()),
+                ],
+            ),
+        ]
